ccd21c7147a713be832c36d0c5160ed0eb897d7f
fahmiibrahimdevs <midragon.project@gmail.com>
2024-12-26 13:45:03 +0000

    update README.md

--- resources/views/components/layouts/layout-top.blade.php
+++ resources/views/components/layouts/layout-top.blade.php
@@ -37,7 +37,7 @@
                     <li class="dropdown">
                         <a href="#" data-toggle="dropdown"
                             class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                            <div class="d-sm-none d-lg-inline-block">Hi, Fahmi Ibrahim</div>
+                            <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
                             <div class="dropdown-title">Logged in 5 min ago</div>
